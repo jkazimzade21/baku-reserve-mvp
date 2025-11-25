e6604b00891bd4cf84672304e678546c3ef25f7e
Jamil <jkazimzade1@gmail.com>
2025-11-24 19:52:07 -0600

    [mobile] fix color-convert metro resolve

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { resolve } = require('metro-resolver');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const projectRoot = __dirname;
@@ -15,5 +16,23 @@ const extraNodeModules = {
 };
 
 config.resolver.extraNodeModules = extraNodeModules;
+
+// Work around Metro failing to resolve the relative import used by color-convert
+// (dependency of chalk/ansi-styles). The file exists as conversions.js, so if
+// Metro can't find it we point directly to the correct path.
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  const isColorConvertRoute = /node_modules[\\/]+color-convert[\\/]+route\.js$/.test(
+    context.originModulePath,
+  );
+
+  if (isColorConvertRoute && moduleName === './conversions') {
+    return {
+      type: 'sourceFile',
+      filePath: path.join(path.dirname(context.originModulePath), 'conversions.js'),
+    };
+  }
+
+  return resolve(context, moduleName, platform);
+};
 
 module.exports = config;
