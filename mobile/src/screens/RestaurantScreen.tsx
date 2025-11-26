744517b95f5968e2dc93a51eed69ea1bafa1b700
Jamil <jkazimzade1@gmail.com>
2025-11-26 10:33:23 -0600

    Persist time selection when tapping chips

--- mobile/src/screens/RestaurantScreen.tsx
+++ mobile/src/screens/RestaurantScreen.tsx
@@ -241,6 +241,7 @@ export default function RestaurantScreen({ route, navigation }: Props) {
   }, []);
 
   const handleTimeSelect = useCallback((time: string) => {
+    setSelectedTime(time);
     setPendingTime(time);
     Haptics.selectionAsync().catch(() => {});
   }, []);
